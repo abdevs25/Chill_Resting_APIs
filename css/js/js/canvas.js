fd4177f77b535aada82f65c14872157a52b97fc8
archisha <ari.min0615@gmail.com>
2026-02-13 18:56:25 +0530

    branch dupli

---
+++ css/js/js/canvas.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1080;
+canvas.height = 1920;
+
+const bgImg = new Image();
+const qrImg = new Image();
+
+bgImg.src = localStorage.getItem("userWallpaper");
+qrImg.src = localStorage.getItem("qrImage");
+
+bgImg.onload = function () {
+  ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+
+  qrImg.onload = function () {
+    ctx.drawImage(qrImg, 390, 1250, 300, 300);
+
+    ctx.font = "36px Arial";
+    ctx.fillStyle = "#000";
+    ctx.textAlign = "center";
+    ctx.fillText("Scan for Emergency Info", 540, 1200);
+  };
+};
+
+function downloadImage() {
+  const link = document.createElement("a");
+  link.download = "emergency-lockscreen.png";
+  link.href = canvas.toDataURL("image/png");
+  link.click();
+}
--- js/qr.js
+++
@@ -1,8 +0,0 @@
-const uniqueId = crypto.randomUUID().slice(0, 8);
-localStorage.setItem("emergencyId", uniqueId);
-
-const emergencyURL = `emergency.html?id=${uniqueId}`;
-
-QRCode.toDataURL(emergencyURL, { width: 300 }, (err, url) => {
-  localStorage.setItem("qrImage", url);
-});
